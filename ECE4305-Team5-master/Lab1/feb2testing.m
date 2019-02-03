ebe1e076a8b4ce769dc80c3f68650e1ea21950cc
Ryano647 <ryano647@gmail.com>
2019-02-03 16:35:46 -0500

    Revert "Merge branch 'master' of https://github.com/Ryano647/ECE4305-Team5"
    This reverts commit c5ec3c2ea10b5231831a867e7acea91fc66dbc53, reversing
    changes made to df472041b495dec7f7c407c601135217854affac.

--- ECE4305-Team5-master/Lab1/feb2testing.m
+++
@@ -1,7 +0,0 @@
-zFittedTxData = zeros(1,frameSize*samplesPerSymbol);
-zFittedRxData = zeros(1,frameSize*samplesPerSymbol);
-for w= 1:frameSize
-    zFittedTxData(samplesPerSymbol*(w-1)+1) = modulatedData(w);
-    zFittedRxData(samplesPerSymbol*(w-1)+1) = allDownsampledRxData(w);
-end
-
--- Lab1/lab1v2.m
+++ Lab1/lab1v2.m
@@ -4,33 +4,33 @@ close all
 sampleRateHz = 1e6; % Sample rate
 samplesPerSymbol = 8;
 frameSize = 8;
-numFrames = 4;
+numFrames = 1;
 numSamples = numFrames*frameSize; % Samples to simulate
 modulationOrder = 2;
 filterSymbolSpan = 4;
 rollOff = 0.2; %default 0.2
 
 %% Visuals
-% cdPre = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
-%     'Name','Baseband');
-% cdPost = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
-%     'SymbolsToDisplaySource','Property',...
-%     'SymbolsToDisplay',frameSize/2,...
-%     'Name','Baseband with Timing Offset');
-% cdPre.Position(1) = 50;
-% cdPost.Position(1) = cdPre.Position(1)+cdPre.Position(3)+10;% Place side by side
+cdPre = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'Name','Baseband');
+cdPost = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'SymbolsToDisplaySource','Property',...
+    'SymbolsToDisplay',frameSize/2,...
+    'Name','Baseband with Timing Offset');
+cdPre.Position(1) = 50;
+cdPost.Position(1) = cdPre.Position(1)+cdPre.Position(3)+10;% Place side by side
 
 %% Impairments
-snr = 200;
+snr = 15;
 timingOffset = samplesPerSymbol*0.01; % Samples
 
 %% Generate symbols
 data = randi([0 modulationOrder-1], numSamples, 1);
 %data = [1,0,0,0,0,0,0,0]';
 % allData = cat(2,allData,data);
-mod = comm.BPSKModulator();
-demod = comm.BPSKDemodulator();
+mod = comm.DBPSKModulator();
 modulatedData = mod.step(data);
+
 %% Add TX/RX Filters
 TxFlt = comm.RaisedCosineTransmitFilter(...
     'OutputSamplesPerSymbol', samplesPerSymbol,...
@@ -63,9 +63,11 @@ filteredData = [];
 allFilteredDatawoff = [];
 allFilteredData = [];
 %timeIndex = 0;
+cd = comm.ConstellationDiagram;
+ 
 for k=1:frameSize:(numSamples)
     
-
+      disp(k);
 %     if k == (25*frameSize+1)
 %         djssd=1;
 %     elseif k == (50*frameSize+1)
@@ -81,106 +83,49 @@ for k=1:frameSize:(numSamples)
     
     % Pass through channel
     noisyData = step(chan, filteredTXData);
+    cd(filteredTXData);
     
     % Time delay signal
     offsetData = step(varDelay, noisyData, k/frameSize*timingOffset); % Variable delay
     
-    % Matched Filter signal
+    % Filter signal
     filteredData = step(RxFlt, offsetData);
 
     filteredDataRef = step(RxFltRef, noisyData);
-    
-    %PLL
-    
-    
-    %demod
-    %by Sean Brady
-       
-    demodulatedData = demod.step(filteredDataRef);
-    
-    
     if k == 1 
         allFilteredDatawoff= filteredData;
         allFilteredData = filteredDataRef;
-        allFilteredTXData = filteredTXData;
-        allnoisyData = noisyData;
-        alldemodulatedData = demodulatedData;
     end
-    if k >1
     allFilteredDatawoff = cat(2,allFilteredDatawoff,filteredData);
     allFilteredData = cat(2,allFilteredData,filteredDataRef);
-    allFilteredTXData = cat(2,allFilteredTXData,filteredTXData);
-    allnoisyData = cat(2,allnoisyData,noisyData);
-    alldemodulatedData = cat(2,alldemodulatedData,demodulatedData);
-    end
+    
     % Visualize Error
-%     step(cdPre,filteredDataRef);
-%     step(cdPost,filteredData);pause(0.1); 
+    step(cdPre,filteredDataRef);
+    step(cdPost,filteredData);pause(0.1); 
     
 end
-
 
 %% Transmit and Receive Plots
 %By R O'brian
 
-timeIndex = timeIndex / (1e6);
-
-
-%Code for creating the correct amountdata points to plot Tx and Rx plots
-%Sean Brady
-
-%Constants
-Spaced_out_recieved_data=[];
-Spaced_out_transmitted_data = [];
-longTx = [];
-longNoise = [];
-%creating a time variable to match the size we need to plot Tx and Rx
-%filtered data
-tas = 1:(numSamples-1)/((frameSize*samplesPerSymbol*numFrames)-1):numSamples;
-
-for Frame_iterator= 1:numFrames
-    Symbols_in_frame_iterator = 1;
-    %Getting all of filteredTXData and noisydata in 1 array
-    for All_sample_iterator= 1:(samplesPerSymbol*frameSize)
-        longTx=[longTx, allFilteredTXData(All_sample_iterator,Frame_iterator)];
-        longNoise = [longNoise, allnoisyData(All_sample_iterator,Frame_iterator)];
-    end 
-    %creating a array with properly spaced filteredData points to plot with
-    %filterTX data
-    for Symbols_in_frame_iterator= 1:frameSize
-        hjk = 1;
-        if Symbols_in_frame_iterator ==1
-            Spaced_out_recieved_data(((Frame_iterator-1)*frameSize*samplesPerSymbol)+1)...
-                = allFilteredData(1,Frame_iterator);
-            Spaced_out_transmitted_data(((Frame_iterator-1)*frameSize*samplesPerSymbol)+1)...
-                = modulatedData(1+(samplesPerSymbol*(Frame_iterator-1)));
-
-            for hjk =1:(samplesPerSymbol-1)
-                Spaced_out_recieved_data = [Spaced_out_recieved_data,0];
-                Spaced_out_transmitted_data = [Spaced_out_transmitted_data,0];
-            end
-        else
-            Spaced_out_recieved_data((samplesPerSymbol*(Symbols_in_frame_iterator-1)+...
-                ((Frame_iterator-1)*frameSize*samplesPerSymbol)+1))...
-                = allFilteredData(Symbols_in_frame_iterator,Frame_iterator);
-            Spaced_out_transmitted_data((samplesPerSymbol*(Symbols_in_frame_iterator-1)+...
-                ((Frame_iterator-1)*frameSize*samplesPerSymbol)+1))...
-                = modulatedData(Symbols_in_frame_iterator+(samplesPerSymbol*(Frame_iterator-1)));
-            for hjk =1:(samplesPerSymbol-1)
-                Spaced_out_recieved_data = [Spaced_out_recieved_data,0];
-                Spaced_out_transmitted_data = [Spaced_out_transmitted_data,0];
-            end
-        end 
-    end
+ timeIndex = timeIndex / (1e6);
+ tas = 1:7/63:8;
+ jksd=[];
+for ids= 1:8
+    if ids ==1
+        jksd(1)= real(allFilteredData(1));
+        jksd = [jksd,0,0,0,0,0,0,0];
+    else
+        jksd((8*(ids-1)+1))=real(allFilteredData(ids));
+        jksd = [jksd,0,0,0,0,0,0,0];
+    end  
 end
 
-
-
- stem(tas, real(Spaced_out_recieved_data));
+ plot(tas, jksd,'-o','MarkerIndices',1:length(jksd));
  hold on;
- plot(tas, real(longTx),'-o','MarkerIndices',1:length(filteredTXData));
+ plot(tas, filteredTXData,'-o','MarkerIndices',1:length(filteredTXData));
  hold on;
- plot(tas, real(longNoise),'-o','MarkerIndices',1:length(noisyData));
+ plot(tas, noisyData,'-o','MarkerIndices',1:length(noisyData));
  hold off;
  title('Transmit and Receive Plot')
  xlabel('Time (ms)')
