a3aeff3bad4c81709c360b4f59c8cb40a9ab3c09
Ryano647 <ryano647@gmail.com>
2019-02-04 12:41:15 -0500

    Fixed File Paths

---
+++ Old Files/Lab1/lab1_EVM.m
@@ -0,0 +1,151 @@
+close all
+
+%% General system details
+sampleRateHz = 1e6; % Sample rate
+samplesPerSymbol = 8;
+frameSize = 8;
+numFrames = 1;
+numSamples = numFrames*frameSize; % Samples to simulate
+modulationOrder = 2;
+filterSymbolSpan = 4;
+rollOff = 0.2; %default 0.2
+
+%% Visuals
+% cdPre = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+%     'Name','Baseband');
+% cdPost = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+%     'SymbolsToDisplaySource','Property',...
+%     'SymbolsToDisplay',frameSize/2,...
+%     'Name','Baseband with Timing Offset');
+% cdPre.Position(1) = 50;
+% cdPost.Position(1) = cdPre.Position(1)+cdPre.Position(3)+10;% Place side by side
+
+%% Impairments
+snr = 15;
+timingOffset = samplesPerSymbol*0.01; % Samples
+
+%% Generate symbols
+data = randi([0 modulationOrder-1], numSamples, 1);
+%data = [1,0,0,0,0,0,0,0]';
+% allData = cat(2,allData,data);
+mod = comm.DBPSKModulator();
+modulatedData = mod.step(data);
+
+%% Add TX/RX Filters
+TxFlt = comm.RaisedCosineTransmitFilter(...
+    'OutputSamplesPerSymbol', samplesPerSymbol,...
+    'FilterSpanInSymbols', filterSymbolSpan,...
+    'RolloffFactor', rollOff);
+
+RxFlt = comm.RaisedCosineReceiveFilter(...
+    'InputSamplesPerSymbol', samplesPerSymbol,...
+    'FilterSpanInSymbols', filterSymbolSpan,...
+    'DecimationFactor', samplesPerSymbol,...
+    'RolloffFactor', rollOff); % Set to filterUpsample/2 when introducing timing estimation
+RxFltRef = clone(RxFlt);
+
+%% Add noise source
+chan = comm.AWGNChannel( ...
+    'NoiseMethod',  'Signal to noise ratio (SNR)', ...
+    'SNR',          snr, ...
+    'SignalPower',  1, ...
+    'RandomStream', 'mt19937ar with seed');
+
+%% Add delay
+varDelay = dsp.VariableFractionalDelay;
+
+%% Setup visualization object(s)
+sa = dsp.SpectrumAnalyzer('SampleRate',sampleRateHz,'ShowLegend',true);
+
+%% Model of error
+% Add timing offset to baseband signal
+filteredData = [];
+allFilteredDatawoff = [];
+allFilteredData = [];
+%timeIndex = 0;
+% cd = comm.ConstellationDiagram;
+ 
+for k=1:frameSize:(numSamples)
+    
+      disp(k);
+%     if k == (25*frameSize+1)
+%         djssd=1;
+%     elseif k == (50*frameSize+1)
+%         djssd=2;
+%     elseif k == (75*frameSize+1)
+%         djssd=3;
+%     end
+    
+    timeIndex = (k:k+frameSize-1).';
+    
+    % Filter signal
+    filteredTXData = step(TxFlt, modulatedData(timeIndex));
+    
+    % Pass through channel
+    noisyData = step(chan, filteredTXData);
+    cd(filteredTXData);
+    
+    % Time delay signal
+    offsetData = step(varDelay, noisyData, k/frameSize*timingOffset); % Variable delay
+    
+    % Filter signal
+    filteredData = step(RxFlt, offsetData);
+
+    filteredDataRef = step(RxFltRef, noisyData);
+    if k == 1 
+        allFilteredDatawoff= filteredData;
+        allFilteredData = filteredDataRef;
+    end
+    allFilteredDatawoff = cat(2,allFilteredDatawoff,filteredData);
+    allFilteredData = cat(2,allFilteredData,filteredDataRef);
+    
+    % Visualize Error
+    step(cdPre,filteredDataRef);
+    step(cdPost,filteredData);pause(0.1); 
+    
+end
+
+%% Transmit and Receive Plots
+
+ timeIndex = timeIndex / (1e6);
+ tas = 1:(numSamples-1)/((frameSize*samplesPerSymbol)-1):numSamples;
+jksd=[];
+for ert= 1:numFrames
+    ids = 1;
+    for ids= 1:frameSize
+        hjk = 1;
+        if ids ==1
+            jksd(((ert-1)*frameSize*samplesPerSymbol)+1)= real(allFilteredData(1,ert+1));
+            for hjk =1:(samplesPerSymbol-1)
+                jksd = [jksd,0];
+            end
+        else
+            jksd((samplesPerSymbol*(ids-1)+((ert-1)*frameSize*samplesPerSymbol)+1))=real(allFilteredData(ids,ert+1));
+            hds(ids)=((samplesPerSymbol*(ids-1))+((ert-1)*frameSize*samplesPerSymbol)+1);
+            for hjk =1:(samplesPerSymbol-1)
+                %
+                jksd = [jksd,0];
+            end
+        end 
+    end
+end
+
+%  plot(tas, jksd,'-o','MarkerIndices',1:length(jksd));
+%  hold on;
+%  plot(tas, filteredTXData,'-o','MarkerIndices',1:length(filteredTXData));
+%  hold on;
+%  plot(tas, noisyData,'-o','MarkerIndices',1:length(noisyData));
+%  hold off;
+%  title('Transmit and Receive Plot')
+%  xlabel('Time (ms)')
+%  ylabel('Amplitude')
+
+
+%% EVM Measurement
+evm = comm.EVM('MaximumEVMOutputPort',true,...
+    'XPercentileEVMOutputPort',true, 'XPercentileValue',90);
+[rmsEVM,maxEVM,pctEVM] = evm(filteredDataRef,filteredData)
+% rmsEM = evm(tas,jksd)
+
+
+
