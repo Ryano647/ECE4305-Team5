b8c2786b11a5a2cd5c1f56ac8d19f4d09e6a08dd
Ryano647 <ryano647@gmail.com>
2019-02-18 19:09:52 -0500

    Actually working for DBPSK

--- ECE4305-Team5-master/Lab2/lab2fft.m
+++ ECE4305-Team5-master/Lab2/lab2fft.m
@@ -8,7 +8,7 @@ modulationOrder = 2;
 filterUpsample = 4; %original value 4
 filterSymbolSpan = 8;
 fftOrder = 2^10;
-
+fftIndex = 1;
 
 %% Impairments
 snr = 15;
@@ -18,7 +18,7 @@ frequencyOffsetHz = (frequencyOffsetHz) * 1;
 
 %% Generate symbols
 data = randi([0 samplesPerSymbol], numSamples, 1);
-mod = comm.QPSKModulator();
+mod = comm.DBPSKModulator();
 modulatedData = mod.step(data);
 
 %% Add TX Filter
@@ -36,12 +36,18 @@ sa = dsp.SpectrumAnalyzer('SampleRate',sampleRateHz,'ShowLegend',true);
 
 % Precalculate constant(s)
 normalizedOffset = 1i.*2*pi*frequencyOffsetHz./sampleRateHz;
+sigNoMod = [];
+offsetEstimates = zeros(floor(length(noisyData)/fftOrder),1);
+indexToHz = sampleRateHz/(modulationOrder*fftOrder);
+corrected = [];
 
+ 
 offsetData = zeros(size(noisyData));
 for k=1:frameSize:numSamples*filterUpsample
 
     % Create phase accurate vector
     timeIndex = (k:k+frameSize-1).';
+    fftIndex = (k:k+fftOrder-1);
     freqShift = exp(normalizedOffset*timeIndex + phaseOffset);
 %     freqShift = cos(normalizedOffset + phaseOffset);
 %     freqShift = exp(normalizedOffset*timeIndex.'); % textbook example
@@ -49,9 +55,21 @@ for k=1:frameSize:numSamples*filterUpsample
     % Offset data and maintain phase between frames
     offsetData(timeIndex) = (noisyData(timeIndex).*freqShift);
 %     offsetData = filteredData.*freqShift; % textbook example
- 
+     sigNoMod = offsetData(timeIndex).^modulationOrder;
+    
+     freqHist = abs(fft(sigNoMod));
+    % Determine most likely offset
+    [~,maxInd] = max(freqHist);
+    offsetInd = maxInd - 1;
+    if maxInd>=fftOrder/2 % Compensate for spectrum shift
+    offsetInd = offsetInd - fftOrder;
+    end
+    % Convert to Hz from normalized frequency index
+    offsetEstimates(k) = offsetInd * indexToHz;
+    corrected = noisyData - offsetData;
+    
     % Visualize Error
-    step(sa,[noisyData(timeIndex),offsetData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
+    step(sa,[noisyData(timeIndex),offsetData(timeIndex),corrected(timeIndex)]);pause(0.1); %#ok<*UNRCH>
 
 end
 %% Plot
@@ -59,11 +77,12 @@ df = sampleRateHz/frameSize;
 frequencies = -sampleRateHz/2:df:sampleRateHz/2-df;
 spec = @(sig) fftshift(10*log10(abs(fft(sig))));
 h = plot(frequencies, spec(noisyData(timeIndex)),...
-     frequencies, spec(offsetData(timeIndex)));
+     frequencies, spec(offsetData(timeIndex)),...
+     frequencies, spec(corrected(timeIndex)));
  
 grid on;
 xlabel('Frequency (Hz)');
 ylabel('PSD (dB)');
-legend('Original','Offset','Location','Best');
+legend('Original','Offset','Corrected','Location','Best');
 NumTicks = 5;L = h(1).Parent.XLim;
 set(h(1).Parent,'XTick',linspace(L(1),L(2),NumTicks))
